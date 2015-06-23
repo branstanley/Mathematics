64100435561cff0bfaf761195b035b1ed36224ca
Brandon Stanley <bstanley@lakeheadu.ca>
2015-06-22 22:42:40 -0400

    Some Derivations
    Needs some work, as I'm just learning.

---
+++ Mathematics/Mathematics/Derivatives.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    static class Derivatives
+    {
+        public delegate Double F(Double x);
+
+        public static Double centered_three_point(F f, Double x)
+        {
+            Double h = 0.000000001;
+            return (f(x+h) - f(x-h))/(2*h);
+        }
+
+        public static Double centered_five_point(F f, Double x)
+        {
+            Double h = 0.000000001;
+            return (f(x-2*h) - 8 * f(x - h) + 8 * f(x + h) - f(x + 2 * h)) / (12 * h);
+        }
+        
+    }
+}
---
+++ Mathematics/Mathematics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Three Point: " + Derivatives.centered_three_point(F, 10));
+            Console.WriteLine("Five Point: " + Derivatives.centered_five_point(F, 10));
+            Console.ReadKey();
+        }
+
+        public static Double F(Double x)
+        {
+            return x * x * x * x * x * x;
+        }
+    }
+}
